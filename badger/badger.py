8bdcfb689c55b8477de293cb07868ffb5b64bdcf
lidijarad <radovanovic.lidija22@gmail.com>
2017-07-31 10:40:14 +0200

    more initial commits

--- badger/badger.py
+++ badger/badger.py
@@ -3,11 +3,12 @@
 import pkg_resources
 
 from xblock.core import XBlock
-from xblock.fields import Scope, Integer
+from xblock.fields import Scope, Integer, String, Float, List, Boolean, ScopeIds
 from xblock.fragment import Fragment
+from xblockutils.studio_editable import StudioEditableXBlockMixin
+from xblockutils.settings import XBlockWithSettingsMixin
 
-
-class BadgerXBlock(XBlock):
+class BadgerXBlock(StudioEditableXBlockMixin, XBlockWithSettingsMixin, XBlock):
     """
     TO-DO: document what your XBlock does.
     """
@@ -16,10 +17,27 @@ class BadgerXBlock(XBlock):
     # self.<fieldname>.
 
     # TO-DO: delete count, and define your own fields.
+    display_name = String(
+        display_name="Display Name",
+        help="This name appears in the horizontal navigation at the top of the page.",
+        scope=Scope.settings,
+        default="Badger"
+    )
+
+    pass_mark = Float(
+        default=80.0, 
+        scope=Scope.settings,
+        help="Minium grade required to award this badge",
+    )
+
     count = Integer(
-        default=0, scope=Scope.user_state,
+        default=0, 
+        scope=Scope.user_state,
         help="A simple counter, to show something happening",
     )
+
+    editable_fields = ('display_name', 'pass_mark',)
+    show_in_read_only_mode = True
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
@@ -38,6 +56,32 @@ class BadgerXBlock(XBlock):
         frag.add_javascript(self.resource_string("static/js/src/badger.js"))
         frag.initialize_js('BadgerXBlock')
         return frag
+
+
+    def studio_view(self, context):
+        """
+        Render a form for editing this XBlock
+        """
+        frag = Fragment()
+        context = {'fields': [],
+                    'xblock_list': self.xblock_list,
+                  }
+        # Build a list of all the fields that can be edited:
+        for field_name in self.editable_fields:
+            field = self.fields[field_name]
+            assert field.scope in (Scope.content, Scope.settings), (
+                "Only Scope.content or Scope.settings fields can be used with "
+                "StudioEditableXBlockMixin. Other scopes are for user-specific data and are "
+                "not generally created/configured by content authors in Studio."
+            )
+            field_info = self._make_field_info(field_name, field)
+            if field_info is not None:
+                context["fields"].append(field_info)
+        frag.content = loader.render_django_template("static/html/badger_edit.html", context)
+        frag.add_javascript(loader.load_unicode("static/js/src/badger_edit.js"))
+        frag.initialize_js('StudioEditableXBlockMixin')
+        return frag
+
 
     # TO-DO: change this handler to perform your own actions.  You may need more
     # than one handler, or you may not need any handlers at all.
