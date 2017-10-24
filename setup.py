b8f4187eab98e40720f676ee4a741d9804135b69
Nico van Niekerk <nico@proversity.org>
2017-10-24 07:32:10 +0200

    add responsive styling for mobile (#8)
    * make image responsive

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='badger-xblock',
-    version='0.2.1',
+    version='0.2.2',
     description='badger XBlock',   # TODO: write a better description.
     license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
     packages=[
